ad9b094ec96279db23db5c5161a21bdb31d21131
Guilherme <gui_araujo.carvalho@hotmail.com>
2025-01-04 12:24:58 -0300

    Inplementando sistema de validação aoprograma

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def exibir_nome_do_programa():
     print("""
       
@@ -18,24 +20,44 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
+    os.system('clear')
     # os.system('clear')           no linux
     print('Finalizando o app\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    input('Digite uma ENTER para voltar ao menu principal!')
+    main()
 
-    match opcao_escolhida:
-        case 1:
-            print('Cadastrar restaurante')
-        case 2:
-            print('Listar restaurante')
-        case 3:
-            print('Ativar restaurante')
-        case _:
-            finalizar_app()
+def cadastrar_novo_restaurante():
+    os.system('clear')
+    print('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = str(input('Insira o nome do restaurante que deseja cadastrar: '))
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
+    input('Aperte ENTER para voltar ao menu principal!')
+    main()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+
+        match opcao_escolhida:
+            case 1:
+                cadastrar_novo_restaurante()
+            case 2:
+                print('Listar restaurante')
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
